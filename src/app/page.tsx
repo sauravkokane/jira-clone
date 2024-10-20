9fe1f6ead89f65a3723e9b0dd3c60cb67c4f4adc
theSK2001 <sbkok6470@gmail.com>
2024-10-20 18:22:26 +0530

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Test from "@/features/Test";
+
+
+export default function Home() {
+  return (
+   <div >
+    <Button variant="test">Click Me</Button>
+   </div>
+  );
+}

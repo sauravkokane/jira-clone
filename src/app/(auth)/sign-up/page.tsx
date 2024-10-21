f9c79d714b959e4a2ecbeca34759bce355bcc7b0
theSK2001 <sbkok6470@gmail.com>
2024-10-21 23:50:53 +0530

    added sign up and sign in cards and handled forms for them.

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,11 +1,10 @@
+"use client"
+
+import { SignUpCard } from '@/features/auth/components/sign-up-card';
 import React from 'react'
 
 const SignUpPage: React.FC = () => {
-  return (
-    <div>
-      Sign Up Page
-    </div>
-  )
+  return <SignUpCard />
 }
 
 export default SignUpPage;
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,14 +1,19 @@
-import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
-import { z } from "zod";
-import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import {
   Form,
   FormControl,
@@ -16,29 +21,52 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
-import React from "react";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
 const formSchema = z.object({
+  name: z.string().trim().min(1, "Required").max(256),
   email: z.string().trim().email(),
-  password: z.string().min(1, "Required").max(256),
+  password: z.string().min(8, "Minimum 8 characters required.").max(256),
 });
 
+/**
+ * Logs the form values to the console when the form is submitted.
+ *
+ * @param {z.infer<typeof formSchema>} values - The form values.
+ */
 const onSubmit = (values: z.infer<typeof formSchema>) => {
   console.log(values);
 };
-
-export const SignInCard = () => {
+/**
+ * A form card for signing up.
+ *
+ * @returns A card component with a signup form.
+ */
+export const SignUpCard = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      name: "",
       email: "",
       password: "",
     },
   });
+
   return (
     <Card className="h-full w-full md:w-[487px] border-none shadow-none">
       <CardHeader className="flex justify-center items-center text-center p-7">
-        <CardTitle className="text-2xl">Welcome Back!</CardTitle>
+        <CardTitle className="text-2xl">Sign Up Page</CardTitle>
+        <CardDescription>
+          By Signing up you agree to our{" "}
+          <Link href="/privacy">
+            <span className="text-blue-700">Privacy Policy</span>
+          </Link>{" "}
+          and{" "}
+          <Link href="/terms">
+            <span className="text-blue-700">Terms of Service</span>
+          </Link>{" "}
+        </CardDescription>
       </CardHeader>
       <div className="px-7">
         <DottedSeparator />
@@ -46,6 +74,22 @@ export const SignInCard = () => {
       <CardContent className="p-7">
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              name="name"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="text"
+                      placeholder="Enter your name"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
             <FormField
               name="email"
               control={form.control}
@@ -56,6 +100,7 @@ export const SignInCard = () => {
                       {...field}
                       type="email"
                       placeholder="Enter email address"
+                      disabled={false}
                     />
                   </FormControl>
                   <FormMessage />
@@ -78,6 +123,7 @@ export const SignInCard = () => {
                 </FormItem>
               )}
             />
+
             <Button disabled={false} size={"lg"} className="w-full">
               Login
             </Button>

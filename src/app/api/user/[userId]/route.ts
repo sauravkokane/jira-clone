dc894a71defb06343a3af7be76ebcfcfca1f2503
theSK2001 <sbkok6470@gmail.com>
2024-10-22 23:42:26 +0530

    added api routes to project. and added hono library for back end in project.

---
+++ src/app/api/user/[userId]/route.ts
@@ -0,0 +1,7 @@
+export function GET(
+    req: Request,
+    {params}: {params: {userId: string}}
+) {
+
+    return Response.json({userId: params.userId})
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
-
-
 export default function Home() {
   return (
-   <div >
-    <Button size={"xs"}>Primary</Button>
-    <Button variant={"secondary"}>Secondary</Button>
-    <Button variant={"destructive"}>Destructive</Button>
-    <Button variant={"outline"}>Outline</Button>
-    <Button variant={"ghost"}>Ghost</Button>
-    <Button variant={"muted"}>Muted</Button>
-    <Button variant={"teritary"}>Teritary</Button>
-    <Input/>
-   </div>
+    <div>
+      <Button size={"xs"}>Primary</Button>
+      <Button variant={"secondary"}>Secondary</Button>
+      <Button variant={"destructive"}>Destructive</Button>
+      <Button variant={"outline"}>Outline</Button>
+      <Button variant={"ghost"}>Ghost</Button>
+      <Button variant={"muted"}>Muted</Button>
+      <Button variant={"teritary"}>Teritary</Button>
+      <Input />
+    </div>
   );
 }
